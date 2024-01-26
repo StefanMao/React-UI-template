1d7b341a68b8e4931f69bf4021f060bd15f48b27
stefanmao <stefanmao1112@gmail.com>
2024-01-27 00:43:21 +0800

    fix: PriceInputTextField can't pass value to react hook form

--- web/src/components/ageGroupPriceList/priceInput/PriceInput.tsx
+++ web/src/components/ageGroupPriceList/priceInput/PriceInput.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { useController, UseControllerProps } from 'react-hook-form';
-import {InputAdornment } from '@mui/material';
+import { InputAdornment } from '@mui/material';
 
 import { PriceInputContainer, PriceInputTextField, FieldSmallDescription } from './PriceInputStyle';
 import { addComma } from '../../../utils/utils';
 
 type FormValues = {
   priceInput: string;
+  ageStart: string;
 };
 
 interface PriceInputProps extends UseControllerProps<FormValues> {}
@@ -15,35 +16,43 @@ export const useHook = (props: UseControllerProps<FormValues>) => {
   const {
     field,
     fieldState: { invalid, error },
-  } = useController({ ...props });
+  } = useController(props);
 
   // To do : Add validation logic and other error msg
   // const isNotNumeric = !/^-?\d+(\.\d*)?$/.test(field.value);
 
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+
+    const unformattedValue = event.target.value.replace(/,/g, '');
+    field.onChange(unformattedValue);
+  };
+
   const states = { invalid, error };
-  const actions = {};
+  const actions = { handleChange };
   return [states, actions, field] as const;
 };
 
 const PriceInput: React.FC<PriceInputProps> = (props) => {
   const [states, actions, field] = useHook(props);
   const { invalid, error } = states;
+  const { handleChange } = actions;
 
   return (
     <PriceInputContainer>
       <FieldSmallDescription align="left">入住費用(每人每晚)</FieldSmallDescription>
       <PriceInputTextField
-        size="small"
         {...field}
+        size="small"
         variant="outlined"
         fullWidth
         placeholder="請輸入費用"
-        value={field.value === '' ? '' : addComma(field.value)}
+        value={addComma(String(field.value))}
         error={invalid}
         helperText={invalid ? error?.message || '不可以為空白' : ''}
         InputProps={{
           startAdornment: <InputAdornment position="start">TWD</InputAdornment>,
         }}
+        onChange={handleChange}
       />
       <FieldSmallDescription align="right">輸入0表示免費</FieldSmallDescription>
     </PriceInputContainer>
